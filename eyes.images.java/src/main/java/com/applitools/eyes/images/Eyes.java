03187b64663e04039ba2de45e4dc67b6b4afed24
Daniel Puterman <daniel.puterman@applitools.com>
2019-02-17 23:09:48 +0200

    Version up.

--- eyes.images.java/src/main/java/com/applitools/eyes/images/Eyes.java
+++ eyes.images.java/src/main/java/com/applitools/eyes/images/Eyes.java
@@ -29,7 +29,7 @@ public class Eyes extends EyesBase {
      */
     @Override
     public String getBaseAgentId() {
-        return "eyes.images.java/4.0.0";
+        return "eyes.images.java/4.0.1";
     }
 
     @Override
